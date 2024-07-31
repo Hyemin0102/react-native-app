cb31fc426b6599eee06687d6bd50d0e15ee00b69
hyemin.jo <gkdlt7373@gmail.com>
2024-07-31 19:13:10 +0900

    스크롤뷰 연습

--- App.tsx
+++ App.tsx
@@ -1,32 +1,75 @@
 import React, {useState} from 'react';
-import {Button, SafeAreaView, Text, View} from 'react-native';
-import {ThemeContext} from './context/ThemeContext.js';
-import HomeScreen from './components/HomeScreen/HomeScreen.js';
-import useToggle from './customHooks/useToggle.js';
-function App(): React.JSX.Element {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+import {Image, ScrollView, SafeAreaView, TextInput, View} from 'react-native';
 
-  //토글로 값 전환해주는 커스텀훅
-  const [isOn, toggleIsOn] = useToggle(false);
-  const toggleTheme = () => {
-    setIsDarkMode(!isDarkMode);
-  };
+function App(): React.JSX.Element {
+  // const [imageSource, setImageSource] = useState({uri: 'https://example.com'});
+  const [textValue, setTextValue] = useState('');
+  const [pwValue, setPwValue] = useState('');
 
   return (
-    //컨텍스트 감싸기, 전달할 값은 다크모드 상태
-    <ThemeContext.Provider value={isDarkMode}>
-      <SafeAreaView>
-        <View style={{backgroundColor: isDarkMode ? '#222222' : 'white'}}>
-          <Text style={{color: isDarkMode ? 'white' : '#222222'}}>
-            안녕하세요
-          </Text>
-          <Button title={'모드 변경'} onPress={toggleTheme} />
-        </View>
-        <HomeScreen />
-        <Button title={'Toggle'} onPress={toggleIsOn} />
-        <Text>{isOn ? 'ON' : 'OFF'}</Text>
-      </SafeAreaView>
-    </ThemeContext.Provider>
+    <SafeAreaView>
+      {/*<Image*/}
+      {/*  source={imageSource}*/}
+      {/*  style={{width: 200, height: 200, backgroundColor: 'gray'}}*/}
+      {/*  resizeMode={'cover'}*/}
+      {/*  onError={() => {*/}
+      {/*    console.log('에러 이미지');*/}
+      {/*    setImageSource(require('./assets/images/appDown_sect02_img.webp'));*/}
+      {/*  }}*/}
+      {/*/>*/}
+      {/*<Image*/}
+      {/*  source={{*/}
+      {/*    uri: 'https://i.pinimg.com/474x/d9/b5/df/d9b5dfebade0f8b407f0559d257cfc7c.jpg',*/}
+      {/*  }}*/}
+      {/*  style={{width: 200, height: 200}}*/}
+      {/*  resizeMode={'cover'}*/}
+      {/*/>*/}
+      <TextInput
+        style={{borderWidth: 1, margin: 20, padding: 10, borderRadius: 8}}
+        value={textValue}
+        onChangeText={value => setTextValue(value)}
+        placeholder={'이름을 입력해주세요!'}
+        returnKeyType={'search'}
+      />
+      <TextInput
+        style={{borderWidth: 1, margin: 20, padding: 10, borderRadius: 8}}
+        value={pwValue}
+        onChangeText={value => setPwValue(value)}
+        secureTextEntry={true}
+        placeholder={'비밀번호를 입력해주세요'}
+      />
+      <View style={{borderWidth: 3}}>
+        <ScrollView
+          onScroll={() => {
+            console.log('사용자가 스크롤할 때 발생하는 상황 제어할수있음');
+          }}
+          contentContainerStyle={{backgroundColor: 'gray'}}
+          showsVerticalScrollIndicator={true}
+          horizontal={true}>
+          <Image
+            source={{
+              uri: 'https://i.pinimg.com/474x/d9/b5/df/d9b5dfebade0f8b407f0559d257cfc7c.jpg',
+            }}
+            style={{width: 500, height: 500}}
+            resizeMode={'contain'}
+          />
+          <Image
+            source={{
+              uri: 'https://i.pinimg.com/474x/d9/b5/df/d9b5dfebade0f8b407f0559d257cfc7c.jpg',
+            }}
+            style={{width: 500, height: 500}}
+            resizeMode={'contain'}
+          />
+          <Image
+            source={{
+              uri: 'https://i.pinimg.com/474x/d9/b5/df/d9b5dfebade0f8b407f0559d257cfc7c.jpg',
+            }}
+            style={{width: 500, height: 500}}
+            resizeMode={'contain'}
+          />
+        </ScrollView>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -41,4 +84,13 @@ View 컴포넌트란?
 Props & PropsType
 - props 의 타입 설정하고 타입 맞지 않는 경우 에러 발생시킴
 
+returnKeyType
+done-완료
+go-검색이나 다른 작업에 사용될 때 이동키
+next-입력 필드 여러개
+search-입력 검색
+
+✅
+스크롤뷰의 contentContainerStyle -> 내부 콘텐츠 감싸는 뷰의 스타일 지정.
+테두리는 부모컨텐츠에서 감싸서 사용해야함
  */
