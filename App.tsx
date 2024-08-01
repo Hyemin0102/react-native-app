424aa7691b526e0fa7b3d958864f8bcb2f137252
hyemin.jo <gkdlt7373@gmail.com>
2024-08-01 13:36:10 +0900

    Button

--- App.tsx
+++ App.tsx
@@ -1,13 +1,60 @@
 import React, {useState} from 'react';
-import {Image, ScrollView, SafeAreaView, TextInput, View} from 'react-native';
+import {
+  Text,
+  ScrollView,
+  SafeAreaView,
+  TextInput,
+  Button,
+  Pressable,
+} from 'react-native';
 
 function App(): React.JSX.Element {
   // const [imageSource, setImageSource] = useState({uri: 'https://example.com'});
-  const [textValue, setTextValue] = useState('');
-  const [pwValue, setPwValue] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   return (
     <SafeAreaView>
+      <ScrollView>
+        <TextInput
+          value={email}
+          keyboardType={'email-address'}
+          style={{borderWidth: 1, borderRadius: 4, padding: 10}}
+          placeholder={'이메일을 입력하세요'}
+          onChangeText={value => {
+            setEmail(value);
+          }}
+        />
+        <TextInput
+          value={password}
+          secureTextEntry={true}
+          style={{borderWidth: 1, borderRadius: 4, padding: 10}}
+          placeholder={'비밀번호 입력하세요'}
+          onChangeText={value => {
+            setPassword(value);
+          }}
+        />
+        <Pressable
+          disabled={email.length === 0 || password.length < 4}
+          style={[
+            {backgroundColor: 'black'},
+            (email.length === 0 || password.length < 4) && {opacity: 0.5},
+          ]}
+          onPress={() => {
+            console.log('clicked');
+            console.log(email, password);
+          }}>
+          <Text
+            style={{
+              color: 'white',
+              padding: 10,
+              textAlign: 'center',
+            }}>
+            submit(Pressable)
+          </Text>
+        </Pressable>
+        {/*<Button title={'submit'} color={'black'} />*/}
+      </ScrollView>
       {/*<Image*/}
       {/*  source={imageSource}*/}
       {/*  style={{width: 200, height: 200, backgroundColor: 'gray'}}*/}
@@ -24,51 +71,51 @@ function App(): React.JSX.Element {
       {/*  style={{width: 200, height: 200}}*/}
       {/*  resizeMode={'cover'}*/}
       {/*/>*/}
-      <TextInput
-        style={{borderWidth: 1, margin: 20, padding: 10, borderRadius: 8}}
-        value={textValue}
-        onChangeText={value => setTextValue(value)}
-        placeholder={'이름을 입력해주세요!'}
-        returnKeyType={'search'}
-      />
-      <TextInput
-        style={{borderWidth: 1, margin: 20, padding: 10, borderRadius: 8}}
-        value={pwValue}
-        onChangeText={value => setPwValue(value)}
-        secureTextEntry={true}
-        placeholder={'비밀번호를 입력해주세요'}
-      />
-      <View style={{borderWidth: 3}}>
-        <ScrollView
-          onScroll={() => {
-            console.log('사용자가 스크롤할 때 발생하는 상황 제어할수있음');
-          }}
-          contentContainerStyle={{backgroundColor: 'gray'}}
-          showsVerticalScrollIndicator={true}
-          horizontal={true}>
-          <Image
-            source={{
-              uri: 'https://i.pinimg.com/474x/d9/b5/df/d9b5dfebade0f8b407f0559d257cfc7c.jpg',
-            }}
-            style={{width: 500, height: 500}}
-            resizeMode={'contain'}
-          />
-          <Image
-            source={{
-              uri: 'https://i.pinimg.com/474x/d9/b5/df/d9b5dfebade0f8b407f0559d257cfc7c.jpg',
-            }}
-            style={{width: 500, height: 500}}
-            resizeMode={'contain'}
-          />
-          <Image
-            source={{
-              uri: 'https://i.pinimg.com/474x/d9/b5/df/d9b5dfebade0f8b407f0559d257cfc7c.jpg',
-            }}
-            style={{width: 500, height: 500}}
-            resizeMode={'contain'}
-          />
-        </ScrollView>
-      </View>
+      {/*<TextInput*/}
+      {/*  style={{borderWidth: 1, margin: 20, padding: 10, borderRadius: 8}}*/}
+      {/*  value={textValue}*/}
+      {/*  onChangeText={value => setTextValue(value)}*/}
+      {/*  placeholder={'이름을 입력해주세요!'}*/}
+      {/*  returnKeyType={'search'}*/}
+      {/*/>*/}
+      {/*<TextInput*/}
+      {/*  style={{borderWidth: 1, margin: 20, padding: 10, borderRadius: 8}}*/}
+      {/*  value={pwValue}*/}
+      {/*  onChangeText={value => setPwValue(value)}*/}
+      {/*  secureTextEntry={true}*/}
+      {/*  placeholder={'비밀번호를 입력해주세요'}*/}
+      {/*/>*/}
+      {/*<View style={{borderWidth: 3}}>*/}
+      {/*  <ScrollView*/}
+      {/*    onScroll={() => {*/}
+      {/*      console.log('사용자가 스크롤할 때 발생하는 상황 제어할수있음');*/}
+      {/*    }}*/}
+      {/*    contentContainerStyle={{backgroundColor: 'gray'}}*/}
+      {/*    showsVerticalScrollIndicator={true}*/}
+      {/*    horizontal={true}>*/}
+      {/*    <Image*/}
+      {/*      source={{*/}
+      {/*        uri: 'https://i.pinimg.com/474x/d9/b5/df/d9b5dfebade0f8b407f0559d257cfc7c.jpg',*/}
+      {/*      }}*/}
+      {/*      style={{width: 500, height: 500}}*/}
+      {/*      resizeMode={'contain'}*/}
+      {/*    />*/}
+      {/*    <Image*/}
+      {/*      source={{*/}
+      {/*        uri: 'https://i.pinimg.com/474x/d9/b5/df/d9b5dfebade0f8b407f0559d257cfc7c.jpg',*/}
+      {/*      }}*/}
+      {/*      style={{width: 500, height: 500}}*/}
+      {/*      resizeMode={'contain'}*/}
+      {/*    />*/}
+      {/*    <Image*/}
+      {/*      source={{*/}
+      {/*        uri: 'https://i.pinimg.com/474x/d9/b5/df/d9b5dfebade0f8b407f0559d257cfc7c.jpg',*/}
+      {/*      }}*/}
+      {/*      style={{width: 500, height: 500}}*/}
+      {/*      resizeMode={'contain'}*/}
+      {/*    />*/}
+      {/*  </ScrollView>*/}
+      {/*</View>*/}
     </SafeAreaView>
   );
 }
